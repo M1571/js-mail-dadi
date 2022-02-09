44a2d3a1bf74074146eb8ac098fe5867e1e559ef
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-09 19:40:55 +0100

    Inserisco mail

---
+++ Mail/mail.js
@@ -0,0 +1,15 @@
+// Chiedo Mail
+var mail = prompt('Inserisci la tua email');
+console.log(mail);
+
+// Accesso consentito a
+var access = ['michelangelo@boolean.it', 'gianluca@boolean.it', 'massimo@boolean.it', 'alessio@boolean.it'];
+
+// 
+
+
+
+
+
+
+// Stampa messaggio
